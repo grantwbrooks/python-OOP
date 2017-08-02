e3fcc55bfeace840e0cc80206e1c700b4fa0ecf9
grantwbrooks <grantwbrooks@gmail.com>
2017-08-02 16:56:18 -0400

    finished bike

---
+++ bike.py
@@ -0,0 +1,30 @@
+class Bike(object):
+    def __init__(self, price, max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+    def displayInfo(self):
+        print "Price:", self.price
+        print "Max Speed:", self.max_speed
+        print "Miles:", self.miles
+        return self
+    def ride(self):
+        print "Riding..."
+        self.miles += 10
+        return self
+    def reverse(self):
+        print "Reversing..."
+        self.miles -= 5
+        return self
+    
+
+
+bike1 = Bike(200, "25mpg")
+bike2 = Bike(250, "30mpg")
+bike3 = Bike(300, "55mpg")
+
+bike1.ride().ride().ride().reverse().displayInfo()
+bike2.ride().ride().reverse().reverse().displayInfo()
+bike3.reverse().reverse().reverse().displayInfo()
+
+    
